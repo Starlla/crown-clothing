378f41ea653f86f8282491604014154c5a233cff
Claire <claire.cy.t@gmail.com>
2021-04-23 13:10:21 -0400

    Add collection-item component

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,12 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import Shop from './pages/shop/shop.component';
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={HomePage} />
+        <Route path="/shop" component={Shop} />
       </Switch>
     </BrowserRouter>
   )
