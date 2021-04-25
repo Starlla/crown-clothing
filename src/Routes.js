47edec2b65ba0ec7770b8c455ea173828501ea78
Claire <claire.cy.t@gmail.com>
2021-04-24 22:56:05 -0400

    add form-input component

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 const Routes = () => {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ const Routes = () => {
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/shop" component={Shop} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </BrowserRouter>
   )
